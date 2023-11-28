e541a1d64ee9a4a5268169a5ae0f4462091d0a8e
hugo forcadel <80644752+Hug-prog@users.noreply.github.com>
2023-11-28 11:07:21 +0100

    fix

--- src/Services/Utils/CounterPrice.ts
+++ src/Services/Utils/CounterPrice.ts
@@ -8,11 +8,7 @@ export const CounterPrice = (
 		if (item.productNumber === 0) {
 			initialValue += item.price
 		} else {
-			if (item.productNumber === 1) {
-				initialValue += item.price + item.price
-			} else {
-				initialValue += item.price * item.productNumber
-			}
+			initialValue += item.price * item.productNumber
 		}
 	})
 
